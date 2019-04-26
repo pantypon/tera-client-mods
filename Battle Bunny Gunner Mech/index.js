3ac65c5e9232756f81d2775307eb1230cf33ad50
Panty <yesterdaywastodayafterall@gmail.com>
2019-04-26 10:29:58 -1000

    Add files via upload

---
+++ Battle Bunny Gunner Mech/index.js
@@ -0,0 +1,3 @@
+module.exports = function Battle_Bunny Gunner Mech(mod) {
+    mod.installGPK("Awaken_Booster.gpk");
+}
---
+++ Pink Fishie Set/index.js
@@ -0,0 +1,4 @@
+module.exports = function Pink_Fishie Set(mod) {
+    mod.installGPK("Fishing_Float.gpk");
+    mod.installGPK("Fishing_Flyrod.gpk");
+}
