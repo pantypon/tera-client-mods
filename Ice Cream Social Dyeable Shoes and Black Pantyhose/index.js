d3adac6053c59d3f8348bbfdeb00171a664f0962
Panty <yesterdaywastodayafterall@gmail.com>
2019-05-09 11:38:54 -1000

    Add files via upload

---
+++ Ice Cream Social Dyeable Shoes and Black Pantyhose/index.js
@@ -0,0 +1,3 @@
+module.exports = function Ice_Cream_Social_Dyeable_Shoes_and_Black_Pantyhose(mod) {
+    mod.installGPK("PC_Event_46.gpk");
+}
---
+++ Ice Cream Social Dyeable Shoes and White Pantyhose/index.js
@@ -0,0 +1,3 @@
+module.exports = function Ice_Cream_Social_Dyeable_Shoes_and_White_Pantyhose(mod) {
+    mod.installGPK("PC_Event_46.gpk");
+}
